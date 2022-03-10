89e214a7e325d03c1806ff4ed112accc01e95049
Anastas Metaxa <metaxaanastas@mail.ru>
2022-03-10 11:13:24 +0600

    Minor optimization of locators

--- src/test/java/ru/netology/page/PurchasePage.java
+++ src/test/java/ru/netology/page/PurchasePage.java
@@ -16,16 +16,15 @@ public class PurchasePage {
     private final SelenideElement creditButton = $(byText("Купить в кредит"));
     private final SelenideElement creditHeading = $(byText("Кредит по данным карты"));
     //Форма для ввода данных, со всеми полями ввода, ошибками полей и кнопкой "Продолжить":
-    //У любого элемента с текстом "Номер карты", родитель должен иметь на любой глубине вложения поле input:
-    private final SelenideElement cardNumberField = $x("//*[text()='Номер карты']/..//input");
+    private final SelenideElement cardNumberField = $("input[placeholder='0000 0000 0000 0000']");
     private final SelenideElement cardNumberFieldError = $x("//*[text()='Номер карты']/..//*[@class='input__sub']");
-    private final SelenideElement monthField = $x("//*[text()='Месяц']/..//input");
+    private final SelenideElement monthField = $("input[placeholder='08']");
     private final SelenideElement monthFieldError = $x("//*[text()='Месяц']/..//*[@class='input__sub']");
-    private final SelenideElement yearField = $x("//*[text()='Год']/..//input");
+    private final SelenideElement yearField = $("input[placeholder='22']");
     private final SelenideElement yearFieldError = $x("//*[text()='Год']/..//*[@class='input__sub']");
-    private final SelenideElement ownerField = $x("//*[text()='Владелец']/..//input");
+    private final SelenideElement ownerField = $(byText("Владелец")).parent().$("input");
     private final SelenideElement ownerFieldError = $x("//*[text()='Владелец']/..//*[@class='input__sub']");
-    private final SelenideElement cvcField = $x("//*[text()='CVC/CVV']/..//input");
+    private final SelenideElement cvcField = $("input[placeholder='999']");
     private final SelenideElement cvcFieldError = $x("//*[text()='CVC/CVV']/..//*[@class='input__sub']");
     private final SelenideElement notificationSuccessfully = $(".notification_status_ok");
     private final SelenideElement notificationError = $(".notification_status_error");
